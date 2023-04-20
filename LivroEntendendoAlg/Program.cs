eb5a22763e89928d76c9c98cd142c70c0214a06e
athosmartinez <athosandrade04@gmail.com>
2023-04-19 23:45:28 -0300

    .

--- LivroEntendendoAlg/Program.cs
+++ LivroEntendendoAlg/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-
-namespace MyApp // Note: actual namespace depends on the project name.
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var central = new CentralDeComandos(new Dictionary<string, Action>())
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
 }
